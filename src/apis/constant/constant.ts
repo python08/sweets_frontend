87787ca08801795e6f1c29f668f8104bdb18e38a
Soham <sohamvishwas@gmail.com>
2024-06-12 10:59:38 +0530

    updated server port

--- src/apis/constant/constant.ts
+++ src/apis/constant/constant.ts
@@ -1,3 +1,3 @@
-export const serverUrl = "http://13.201.76.139:1337";
+export const serverUrl = "http://13.201.76.139:443";
 export const welcomeVideoUrl =
   "https://sweettoothbucket.s3.ap-south-1.amazonaws.com/video/Intro.mp4";
